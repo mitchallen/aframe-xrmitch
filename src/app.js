4277a8212efee85d430855229f9116b3486a0953
Mitch Allen <github@mitchallen.com>
2024-04-08 10:27:28 -0400

    feat: added oculus controls (needs work)

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import {moveWhereLooking} from './move-where-looking.js'
 
 console.log("[xrmitch]: running.")
 
+AFRAME.registerComponent('thumbstick-logging',{
+    init: function() {
+        this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+    },
+    logThumbstick: function(evt) {
+        if( evt.detail.y > 0.95 ) { console.log("DOWN"); }
+        if( evt.detail.y < -0.95 ) { console.log("UP"); }
+        if( evt.detail.x > 0.95 ) { console.log("RIGHT"); }
+        if( evt.detail.x < -0.95 ) { console.log("LEFT"); }
+    }
+});
+
+AFRAME.registerComponent('thumbstick-move-where-looking',{
+    init: function() {
+        this.el.addEventListener('thumbstickmoved', this.logThumbstick);
+    },
+    logThumbstick: function(evt) {
+        moveWhereLooking(evt.detail.y);
+    }
+});
+
 document.getElementById('btnForward').addEventListener('click', function (event) {
     moveWhereLooking(-1,0);
     event.preventDefault();
