34394078bb8b2d18a9853867c03770e43a0e0f64
Mitch Allen <github@mitchallen.com>
2024-04-08 06:17:23 -0400

    feat: removed debug statement

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import {moveWhereLooking} from './move-where-looking.js'
 console.log("[xrmitch]: running.")
 
 document.getElementById('btnForward').addEventListener('click', function (event) {
-    alert("DEBUG: FORWARD!")
     moveWhereLooking(-1,0);
     event.preventDefault();
 });
